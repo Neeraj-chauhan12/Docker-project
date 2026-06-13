a57b6129349ee6ad98ae280d6b6bf91c2e594d0d
Neeraj chauhan <nc1676639@gmail.com>
2026-06-13 22:16:57 +0530

    addd ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import Editor from "@monaco-editor/react";
 
 const App = () => {
   return (
-    <div>
-      hello
+    <div className="h-screen w-screen bg-black flex gap-3 px-5">
+      <div className="h-full w-[20vw] bg-gray-800 rounded-2xl "></div>
+      <div className="h-full w-[75vw] bg-gray-600 rounded-2xl ">
+        <Editor height="full" defaultLanguage="javascript" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
