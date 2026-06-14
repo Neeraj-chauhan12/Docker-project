11eab1851c288ad8d30f38de7dfdcca6cf6947e1
Neeraj chauhan <nc1676639@gmail.com>
2026-06-14 15:55:05 +0530

    server connect

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const http=require('http');
+const {Server}=require('socket.io');
+const app=express();
+const server=http.createServer(app)
+
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:5173",
+        credentials:true,
+        methods:["GET","POST"]
+    }
+})
